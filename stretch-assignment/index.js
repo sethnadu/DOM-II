5a9c8c114e666c324855712c2de04c9b06c87ba6
Seth Nadu <seth.nadu@gmail.com>
2019-07-09 19:13:43 -0400

    blocks go to top

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -14,40 +14,67 @@ blocksArray.forEach(item => {
         item.style.cursor = "pointer"
     });
 
-    item.addEventListener("click", () => {
-      item.style.order = "-1";
-     });
+    // item.addEventListener("click", () => {
+    //   if (item.style.order !== "-1") {
+    //       item.style.order = "-1";
+    //   } else if (item.style.order == "-1") {
+    //       item.style.order = "0";
+    //   }
+     
+    //   console.log(item.style.order);
+      
+    //  });
 
 })
 
-// redBlock.addEventListener("click", () => {
-//     item.style.order = "-1";
-//  });
+redBlock.addEventListener("click", () => {
+    redBlock.style.order = "-1";
+    blueBlock.style.order = "0";
+    greenBlock.style.order = "0";
+    pinkBlock.style.order = "0";
+    grayBlock.style.order = "0";
+ });
 
-//  blueBlock.addEventListener("click", () => {
-//     item.style.order = "-1";
-//  });
+ blueBlock.addEventListener("click", () => {
+    blueBlock.style.order = "-1";
+    redBlock.style.order = "0";
+    greenBlock.style.order = "0";
+    pinkBlock.style.order = "0";
+    grayBlock.style.order = "0";
+ });
 
-//  greenBlock.addEventListener("click", () => {
-//     item.style.order = "-1";
-//  });
+ greenBlock.addEventListener("click", () => {
+    greenBlock.style.order = "-1";
+    blueBlock.style.order = "0";
+    redBlock.style.order = "0";
+    pinkBlock.style.order = "0";
+    grayBlock.style.order = "0";
+ });
 
-//  pinkBlock.addEventListener("click", () => {
-//     item.style.order = "-1";
-//  });
+ pinkBlock.addEventListener("click", () => {
+    pinkBlock.style.order = "-1";
+    blueBlock.style.order = "0";
+    greenBlock.style.order = "0";
+    redBlock.style.order = "0";
+    grayBlock.style.order = "0";
+ });
 
-//  grayBlock.addEventListener("click", () => {
-//     item.style.order = "-1";
-//  });
+ grayBlock.addEventListener("click", () => {
+    grayBlock.style.order = "-1";
+    blueBlock.style.order = "0";
+    greenBlock.style.order = "0";
+    pinkBlock.style.order = "0";
+    redBlock.style.order = "0";
+ });
 
 
 // redBlock.addEventListener("mousedown", () => {
-//     TweenLite.to(".block--red", 100, { ease: SlowMo.ease.config(1, 1, false), x: 10000 });
+//     TweenLite.to(".block--red", 100, { ease: SlowMo.ease.config(1, 1, false), x: +1});
 // });
 
-// // redBlock.addEventListener("mouseup", () => {
-// //     TweenLite.to(".block--red", 100, { ease: SlowMo.ease.config(1, 1, false), x: -10000 });
-// // });
+// redBlock.addEventListener("mouseup", () => {
+//     TweenLite.to(".block--red", 100, { ease: SlowMo.ease.config(1, 1, false), x: -10000 });
+// });
 
 // blueBlock.addEventListener("mousedown", () => {
 //     TweenLite.to(".block--blue", 100, { ease: SlowMo.ease.config(1, 1, false), x: 10000 });
