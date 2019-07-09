e7fb41e97d2bf0e1fe7a2b8fd152f7f8404d1afa
Seth Nadu <seth.nadu@gmail.com>
2019-07-09 18:22:46 -0400

    working on stretch project

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,13 +1,63 @@
-let blocksChange = document.querySelectorAll(".blocks");
+let blocksChange = document.querySelectorAll(".block");
 let redBlock = document.querySelector(".block--red");
 let blueBlock = document.querySelector(".block--blue");
 let greenBlock = document.querySelector(".block--green");
 let pinkBlock = document.querySelector(".block--pink");
 let grayBlock = document.querySelector(".block--gray");
 
-blocksChange.forEach((item) => {
-    item.addEventListener('click', event => {
-    console.log(blocksChange.indexOf('item'));
-    });
-});
+let blocksArray = Array.from(blocksChange);
 
+
+blocksArray.forEach(item => {
+
+    item.addEventListener("mouseover", () => {
+        item.style.cursor = "pointer"
+    });
+
+    item.addEventListener("click", () => {
+      item.style.order = "-1";
+     });
+
+})
+
+// redBlock.addEventListener("click", () => {
+//     item.style.order = "-1";
+//  });
+
+//  blueBlock.addEventListener("click", () => {
+//     item.style.order = "-1";
+//  });
+
+//  greenBlock.addEventListener("click", () => {
+//     item.style.order = "-1";
+//  });
+
+//  pinkBlock.addEventListener("click", () => {
+//     item.style.order = "-1";
+//  });
+
+//  grayBlock.addEventListener("click", () => {
+//     item.style.order = "-1";
+//  });
+
+
+// redBlock.addEventListener("mousedown", () => {
+//     TweenLite.to(".block--red", 100, { ease: SlowMo.ease.config(1, 1, false), x: 10000 });
+// });
+
+// // redBlock.addEventListener("mouseup", () => {
+// //     TweenLite.to(".block--red", 100, { ease: SlowMo.ease.config(1, 1, false), x: -10000 });
+// // });
+
+// blueBlock.addEventListener("mousedown", () => {
+//     TweenLite.to(".block--blue", 100, { ease: SlowMo.ease.config(1, 1, false), x: 10000 });
+// });
+// greenBlock.addEventListener("mousedown", () => {
+//     TweenLite.to(".block--green", 100, { ease: SlowMo.ease.config(1, 1, false), x: 10000 });
+// });
+// pinkBlock.addEventListener("mousedown", () => {
+//     TweenLite.to(".block--pink", 100, { ease: SlowMo.ease.config(1, 1, false), x: 10000 });
+// });
+// grayBlock.addEventListener("mousedown", () => {
+//     TweenLite.to(".block--gray", 100, { ease: SlowMo.ease.config(1, 1, false), x: 10000 });
+// });
